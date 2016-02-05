d5f6ba5329c16844dc8f6599114677fd63a76652
MGolars <titorens@gmail.com>
2016-02-05 14:56:00 +0200

    +fix vagrant if exist project

--- vagrant/composter.sh
+++ vagrant/composter.sh
@@ -4,5 +4,12 @@ sudo mv composer.phar /usr/local/bin/composer
 composer config -g github-oauth.github.com a89b572c8d006a10b323ca398d0fee6a784ca2d8
 sudo aptitude install -q -y -f git
 cd /var/www/base
-composer global require "laravel/installer"
-composer create-project laravel/laravel vergo --prefer-dist
+if [ -d ./vergo ]; then
+    cd ./vergo
+    composer update --no-scripts
+    php artisan clear-compiled
+    php artisan optimize
+else
+    composer global require "laravel/installer"
+    composer create-project laravel/laravel vergo --prefer-dist
+fi
