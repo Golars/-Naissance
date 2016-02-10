f5955cc236621defab9f253177c542979bbe3ec6
Selmarinel <selmarinel@gmail.com>
2016-02-10 12:30:44 +0200

    VB-7

---
+++ vergo/app/Modules/VergoBase/Resources/Views/main_dis/dis.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=en>
+    <head>
+        <meta charset=utf-8>
+        <meta http-equiv=X-UA-Compatible content="IE=edge">
+        <meta name=viewport content="width=device-width,initial-scale=1">
+        <meta name=description content="">
+        <meta name=keywords content="">
+        <meta name=author content="">
+        <title>Dis</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+        <script src="./js/bootstrap.min.js"></script>
+        <script src="./js/npm.js"></script>
+    </head>
+    <body>
+        <div class="alert alert-danger">ff</div>
+    </body>
+</html>
